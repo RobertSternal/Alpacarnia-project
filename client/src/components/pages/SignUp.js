655d44008b510ade3f1ebfe99997ab9e1b352f50
RobertSternal <robst@onet.pl>
2023-12-03 00:28:54 +0100

    First Commit - front end

---
+++ client/src/components/pages/SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../../App.css';
+import UserReviews from '../UserReviews';
+
+
+export default function SignUp(){
+    return ( 
+        <>
+        <h1 className="sign-up"> DOŁĄCZ DO NAS </h1>;
+        <UserReviews/>
+        </>
+        );
+}
